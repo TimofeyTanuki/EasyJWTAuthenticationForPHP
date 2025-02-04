f17166aebb02df24e477b0318d90cd8789d3ec57
TimofeyTanuki <76044557+TimofeyTanuki@users.noreply.github.com>
2025-02-04 23:05:52 +0300

    Format updated

--- src/includes/modules/jwt-authentication.php
+++ src/includes/modules/jwt-authentication.php
@@ -1,42 +1,43 @@
 <?php
-	namespace Tanuki\Modules;
 
-	require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/services/jwt-authenticator.php';
+namespace Tanuki\Modules;
 
-	function Main()
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/services/jwt-authenticator.php';
+
+function Main()
+{
+	$Headers = apache_request_headers();
+	
+	if (empty($Headers['Authorization']))
 	{
-		$Headers = apache_request_headers();
-		
-		if (empty($Headers['Authorization']))
-		{
-			http_response_code(401);
-			exit;
-		}
-
-		$Authorization = explode(' ', $Headers['Authorization']);
-		if (count($Authorization) != 2)
-		{
-			http_response_code(401);
-			exit;
-		}
-
-		if ($Authorization[0] !== 'Bearer')
-		{
-			http_response_code(401);
-			exit;
-		}
-
-		// Setting the secret when creating an instance of the authenticator class.
-		$JWTAuthenticator = new Tanuki\Services\JWTAuthenticator('8i^BsP@3-a+*jJk&t7~WY?B:guCU!c#wJDmxMdj^MY?BFxn:;X/LhZC~O>e5R!t+', 'HS256', 900);
-		$Payload = $JWTAuthenticator->Decode($Authorization[1], true, $_SERVER['REQUEST_TIME']);
-
-		if ($Payload === null)
-		{
-			http_response_code(401);
-			exit;
-		}
-
-		define('JWTAuthentication', $Payload);
+		http_response_code(401);
+		exit;
 	}
 
-	Main();
+	$Authorization = explode(' ', $Headers['Authorization']);
+	if (count($Authorization) != 2)
+	{
+		http_response_code(401);
+		exit;
+	}
+
+	if ($Authorization[0] !== 'Bearer')
+	{
+		http_response_code(401);
+		exit;
+	}
+
+	// Setting the secret when creating an instance of the authenticator class.
+	$JWTAuthenticator = new Tanuki\Services\JWTAuthenticator('8i^BsP@3-a+*jJk&t7~WY?B:guCU!c#wJDmxMdj^MY?BFxn:;X/LhZC~O>e5R!t+', 'HS256', 900);
+	$Payload = $JWTAuthenticator->Decode($Authorization[1], true, $_SERVER['REQUEST_TIME']);
+
+	if ($Payload === null)
+	{
+		http_response_code(401);
+		exit;
+	}
+
+	define('JWTAuthentication', $Payload);
+}
+
+Main();
