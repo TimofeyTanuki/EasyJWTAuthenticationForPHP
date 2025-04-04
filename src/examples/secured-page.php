34cbc29d2618135cc8a5a23ca13b379ddea7490e
Timofey Tanuki <76044557+TimofeyTanuki@users.noreply.github.com>
2025-04-04 17:31:10 +0300

    Update secured-page.php

--- src/examples/secured-page.php
+++ src/examples/secured-page.php
@@ -1,5 +1,5 @@
 <?php
-	// If there is not authentication, it will return HTTP 403 (Unauthorized) and exit the script.
+	// If there is no authentication, it will return HTTP 403 (Unauthorized) and exit the script.
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/modules/jwt-authentication.php';
 
 	echo 'This page requires JWT authentication.' . PHP_EOL;
